8f9c5ba7f8479735022daaafcbc79316097c0c00
yume0222 <134505601+yume0222@users.noreply.github.com>
2025-05-12 12:22:30 +0900

    feat: スクロール、ダークモード

--- js/script.js
+++ js/script.js
@@ -11,31 +11,6 @@ darkModeBtn.addEventListener('click', () => {
   }
 });
 
-// 入力した文字を数える
-const text = document.querySelector('#text');
-const count = document.querySelector('#count');
-
-text.addEventListener('keyup', () => {
-  count.textContent = text.value.length;
-  if (text.value.length > 100) {
-    count.classList.add('alert');
-  } else {
-    count.classList.remove('alert');
-  }
-});
-
-// チェックを入れるとボタンを押せるようにする
-const isAgreed = document.querySelector('#check');
-const btn = document.querySelector('#btn');
-
-check.addEventListener('change', () => {
-  if (isAgreed.checked) {
-    btn.disabled = false;
-  } else {
-    btn.disabled = true;
-  }
-});
-
 // ページのスクロール量を表示
 const getScrollPercent = () => {
   const scroll = window.scrollY;
